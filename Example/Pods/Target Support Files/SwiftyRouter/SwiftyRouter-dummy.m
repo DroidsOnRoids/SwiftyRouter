943e2a3c1b05e167a266ea0db85252f51c190262
Piotr Sochalewski <sochalewski@gmail.com>
2016-02-01 14:33:57 +0100

    One more time

---
+++ Example/Pods/Target Support Files/SwiftyRouter/SwiftyRouter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftyRouter : NSObject
+@end
+@implementation PodsDummy_SwiftyRouter
+@end
---
+++ Example/Pods/Target Support Files/SwiftyRouter/SwiftyRouter-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftyRouterVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftyRouterVersionString[];
+
