dd8372b5d02595834ea602dd85e8faab28c58455
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-02-02 13:27:28 +0100

    Parse array

--- Pod/Classes/SwiftyRouter+ObjectMapper.swift
+++ Pod/Classes/SwiftyRouter+ObjectMapper.swift
@@ -11,7 +11,7 @@ import ObjectMapper
 
 public extension Request {
     
-    public func parseObject<T: Mappable>(type: T.Type, completion: SwiftyRouterResult -> Void) -> Self  {
+    private func parse<T: Mappable>(type: T.Type, objectGetter: String -> Any?, completion: SwiftyRouterResult -> Void) -> Self  {
         return response(completionHandler: { (request, response, data, error) -> Void in
             
             if let error = error {
@@ -19,14 +19,22 @@ public extension Request {
             }
             
             if let data = data,
-                json = NSString(data: data, encoding: NSUTF8StringEncoding),
-                object = Mapper<T>().map(json) {
+                json = NSString(data: data, encoding: NSUTF8StringEncoding) as? String,
+                object = objectGetter(json) {
                     completion(SwiftyRouterResult.Success(object))
             }
             
             let parserError = NSError(domain: "com.droidsonroids.SwiftyRouterResult", code: 420, userInfo: ["error": "Parse error."])
             completion(SwiftyRouterResult.Failure(parserError))
         })
+    }
+    
+    public func parseObject<T: Mappable>(type: T.Type, completion: SwiftyRouterResult -> Void) -> Self  {
+        return parse(type, objectGetter: { Mapper<T>().map($0) }, completion: completion)
+    }
+    
+    public func parseArray<T: Mappable>(type: T.Type, completion: SwiftyRouterResult -> Void) -> Self  {
+        return parse(type, objectGetter: { Mapper<T>().mapArray($0) }, completion: completion)
     }
     
 }
